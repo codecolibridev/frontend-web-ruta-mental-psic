c9e60539a3426d377f5040d7d304d56b4c81c93d
RoberthLoorDev <dayvidlife@gmail.com>
2025-11-18 19:14:48 -0500

    REFACTOR: move all files inside src folder 📂

--- src/app/pacientes/page.tsx
+++ src/app/pacientes/page.tsx
@@ -1,13 +1,19 @@
-import PageHeader from '@/components/layout/PageHeader';
+import { PageHeader } from '@/components/index';
 import { Button, PatientsTable } from '@/components/index';
+import { Plus } from 'lucide-react';
 
 export default function page() {
      return (
           <div className="flex flex-col">
                <div className="flex">
                     <PageHeader title="Paciente" description="Información y detalles del paciente" />
-                    <Button variant="primary" type="button" className="h-8 max-w-50 max-h-11 self-center ml-auto">
-                         + Nuevo paciente
+                    <Button
+                         variant="primary"
+                         type="button"
+                         className="h-8 max-w-50 max-h-11 self-center ml-auto"
+                         leftIcon={<Plus className="h-4 w-4" />}
+                    >
+                         Nuevo paciente
                     </Button>
                </div>
                <PatientsTable />

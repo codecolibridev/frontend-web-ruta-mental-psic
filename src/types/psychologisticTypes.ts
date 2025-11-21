1a3354da68b77c7c92ccae9ff98594d4f8612785
RoberthLoorDev <dayvidlife@gmail.com>
2025-11-21 18:33:39 -0500

    ADD: implement usePsychologistic hook and associated service for fetching psychologist data 🚀

---
+++ src/types/psychologisticTypes.ts
@@ -0,0 +1,11 @@
+export interface Psychologistic {
+     id: number;
+     first_name: string;
+     last_name: string;
+     email: string;
+     password: string;
+     role_id: number;
+     created_at: string;
+     updated_at: string | null;
+     deleted_at: string | null;
+}
